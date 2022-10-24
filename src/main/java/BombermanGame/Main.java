f5cac6143c5686378b32fddef5813abb1260e8cc
hduoc2003 <21020059@vnu.edu.vn>
2022-10-24 21:38:37 +0700

    nothing

--- src/main/java/BombermanGame/Main.java
+++ src/main/java/BombermanGame/Main.java
@@ -1,13 +1,16 @@
 package BombermanGame;
 
+import BombermanGame.Sprite.Sprite;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         BombermanGame game = new BombermanGame();
-        game.runGame(args);
     }
 }
